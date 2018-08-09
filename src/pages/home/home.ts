e6e810c7500f7ee15642bc0ad0beadd3e34503c6
goodjun <goodjun@163.com>
2018-08-09 03:41:33 -0700

    test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, normalizeURL } from 'ionic-angular';
+
+import { File } from '@ionic-native/file';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html',
+    providers: [File, FileTransfer]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public logText;
+    public picturePath;
 
-  }
+    constructor(
+        public navCtrl: NavController,
+        private file: File,
+        private transfer: FileTransfer
+    ) {
+
+        this.picturePath = normalizeURL(this.file.cacheDirectory) + 'test.jpg';
+
+    }
+
+    download() {
+
+        const fileTransfer: FileTransferObject = this.transfer.create();
+
+        fileTransfer.download('https://passport4us.sjfood.us/Content/images/login_bg.jpg', this.file.dataDirectory  + 'test.jpg').then(
+            _ => this.logText = 'DOWNLOAD YES'
+        ).catch(
+            err => this.logText = 'DOWNLOAD ERROR'
+        );
+
+    }
+
+    show() {
+        
+        // this.file.checkDir(this.file.dataDirectory, 'mydir').then(
+        //     _ => {
+        //         console.log('Directory exists');
+        //         this.logText = 'Directory exists';
+        //     }
+        // ).catch(
+        //     err => {
+        //         console.log('Directory doesn\'t exist')
+        //         this.logText = 'Directory doesn\'t exist';
+        //     }
+        // );
+
+        // cache： this.file.cacheDirectory
+        
+    }
 
 }
